c3d50e9d601e2faa6a61c6d7811a42c423750bca
ranoob <whyfck@gmail.com>
2023-11-02 17:31:43 -0300

    fix: route base

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,8 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createMemoryHistory } from 'vue-router'
 import HomeView from '@/views/home/_index.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createMemoryHistory('neo-portfolio'),
   linkActiveClass: 'active-link',
   routes: [
     {
